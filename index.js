d5604cad700683533d940a5eaef20b254e36336f
KarmaMichealis <achantayd@gmail.com>
2020-04-15 18:56:49 -0400

    project first push

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
+const principal=200000
+const intrestRate=0.05
 
 
 
